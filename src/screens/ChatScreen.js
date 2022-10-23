65fe143cb076c9ad655780549bfb34ec5068bd03
Alan <yao.han1@northeastern.edu>
2022-10-23 00:32:36 -0400

    update InputBox

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,15 +1,29 @@
-import {View, Text, FlatList } from 'react-native';
-import chats from '../../assets/data/chats.json';
-import ChatListItem from '../components/ChatListItem';
+// use rnfe to create template
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
+import bg from "../../assets/images/BG.png";
+import Message from "../components/Message";
+import InputBox from "../components/InputBox";
+import messages from "../../assets/data/messages.json";
 
 
-const ChatsScreen = () => {
+const ChatScreen = () => {
   return (
-  <FlatList 
-    data={chats}
-    renderItem={({item}) => <ChatListItem chat={item} />}
-    />
-    )
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.bg}>
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
 };
-
-export default ChatsScreen;
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+});
+export default ChatScreen;
